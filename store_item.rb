fff6fbd7296718626d714d1b8ff931c2b80239f5
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-21 15:58:16 -0400

    printed item_1 in hash with rocke t hashes

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+item_1 = {:product => "Apple", :color => "Red", :price => "1.00"}
+item_2 = {product: "Orange", color: "Orange", price: 2.00}
+item_3 = {product: "Banana", color: "Yellow", price: 3.00}
+
+p "The item you are buying is an #{item_1[:product]} that is #{item_1[:color]} in color and will cost #{item_1[:price]}"
