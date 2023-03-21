9bc9897df6a4baf74f5df5a017c2469337702cdf
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-21 17:19:21 -0400

    added print for item_2

--- store_item.rb
+++ store_item.rb
@@ -3,3 +3,6 @@ item_2 = {product: "Orange", color: "Orange", price: 2.00}
 item_3 = {product: "Banana", color: "Yellow", price: 3.00}
 
 p "The item you are buying is an #{item_1[:product]} that is #{item_1[:color]} in color and will cost #{item_1[:price]}"
+
+p "The item you are buying is an #{item_2[:product]} that is #{item_2[:color]} in color and will cost #{item_2[:price]}"
+
